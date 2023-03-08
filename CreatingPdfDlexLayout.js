a8dc2996519fec467d8f6a0f7655ba10048cfc23
Ranganadh <evranganadh@dynamicpdf.com>
2023-03-08 11:20:11 +0530

    Fixed an issue with file extension and file name

--- CreatingPdfDlexLayout.js
+++ CreatingPdfDlexLayout.js
@@ -8,13 +8,13 @@ import {
 
 export class CreatingPdfDlexLayout {
     static async Run() {
-        var layoutData = new LayoutDataResource("C:/temp/dynamicpdf-api-samples/creating-pdf-dlex-layout-endpoint/creating-pdf-dlex-layout.");
+        var layoutData = new LayoutDataResource("C:/temp/dynamicpdf-api-samples/creating-pdf-dlex-layout-endpoint/creating-pdf-dlex-layout.json");
         var dlexEndpoint = new DlexLayout("samples/creating-pdf-dlex-layout-endpoint/creating-pdf-dlex-layout.dlex", layoutData);
         dlexEndpoint.apiKey = "DP.xxx-api-key-xxx";
         var res = await dlexEndpoint.process();
         
         if (res.isSuccessful) {
-            var outFile = "C:/temp/dynamicpdf-api-samples/creating-pdf-dlex-layout-endpoint/create-pdf-dlex-output-nodejs.pdf";
+            var outFile = "C:/temp/dynamicpdf-api-samples/creating-pdf-dlex-layout-endpoint/create-pdf-dlex-layout-output-nodejs.pdf";
             var outStream = fs.createWriteStream(outFile);
             outStream.write(res.content);
             outStream.close();
