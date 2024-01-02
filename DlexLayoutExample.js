ccaa74ad24be44de0b55fc676692fe4d70e32633
James A. Brannan <jamesabrannan@hushmail.com>
2024-01-02 13:41:49 -0500

    changes for dlex-layout enpoint

--- DlexLayoutExample.js
+++ DlexLayoutExample.js
@@ -1,19 +1,48 @@
 import fs from 'fs';
 import {
     LayoutDataResource,
-    DlexLayout
+    DlexLayout,
+    DlexResource
 } from "@dynamicpdf/api"
 
 export class DlexLayoutExample {
+
     static async Run() {
-        var layoutData = new LayoutDataResource("C:/temp/dynamicpdf-api-samples/create-pdf-dlex/SimpleReportWithCoverPage.json");
+        var localPath = "c:/temp/dlex-layout/";
+        var apiKey = "DP.J+G1BiQYH7x+3D7uR5jo+Jo0PPKrY0nU1l+j3XbOtobjTqpWSZErUV70";
+        await this.RunFromCloud(apiKey, localPath);
+        await this.RunFromLocal(apiKey, localPath);
+    }
+
+    static async RunFromCloud(apiKey, localPath) {
+        var layoutData = new LayoutDataResource(localPath + "SimpleReportWithCoverPage.json");
         var dlexEndpoint = new DlexLayout("samples/dlex-layout/SimpleReportWithCoverPage.dlex", layoutData);
-        dlexEndpoint.apiKey = "DP.xxx-api-key-xxx";
+        dlexEndpoint.apiKey = apiKey;
 
         var res = await dlexEndpoint.process();
         
         if (res.isSuccessful) {
-            var outFile = "C:/temp/dynamicpdf-api-samples/create-pdf-dlex/nodejs-dlex-layout-example-output.pdf";
+            var outFile = localPath + "nodejs-dlex-layout-example-output.pdf";
+            var outStream = fs.createWriteStream(outFile);
+            outStream.write(res.content);
+            outStream.close();
+        } else {
+            console.log(res.errorJson);
+        }
+    }
+
+    static async RunFromLocal(apiKey, localPath) {
+        var layoutData = new LayoutDataResource(localPath + "SimpleReportWithCoverPage.json");
+        
+        var dlexResource = new DlexResource(localPath + "SimpleReportWithCoverPage.dlex", "SimpleReportWithCoverPage.dlex");
+        var dlexEndpoint = new DlexLayout(dlexResource, layoutData);
+        dlexEndpoint.dlexAdditionalResource(localPath + "NorthwindLogo.gif", "NorthwindLogo.gif");
+        dlexEndpoint.apiKey = apiKey;
+
+        var res = await dlexEndpoint.process();
+        
+        if (res.isSuccessful) {
+            var outFile = localPath + "nodejs-dlex-layout-example-local-output.pdf";
             var outStream = fs.createWriteStream(outFile);
             outStream.write(res.content);
             outStream.close();
