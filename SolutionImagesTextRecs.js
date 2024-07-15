63ebcaeaeaf83fce55403561967d5f69bea6a6d4
James A. Brannan <jamesabrannan@hushmail.com>
2024-07-15 14:36:01 -0400

    Update SolutionImagesTextRecs.js

--- SolutionImagesTextRecs.js
+++ SolutionImagesTextRecs.js
@@ -1,11 +1,14 @@
 import fs from 'fs';
 import {
     Pdf,
+    PageInput,
     LineElement,
+    ImageResource,
+    ImageElement,
+    RectangleElement,
     elementPlacement,
     LineStyle,
     RgbColor,
-    Font,
     TextElement
 } from "@dynamicpdf/api"
 
@@ -18,29 +21,47 @@ export class SolutionImagesTextRecs {
         var pdf = new Pdf();
         pdf.apiKey = Constants.ApiKey;
 
-        var pageInput = pdf.addPage(1008, 612);
-        
-        var textElement = new TextElement("Hello PDF", elementPlacement.topCenter, -50, 100);
+        var pageInput = pdf.addPage(1008,612);
+        var textElement = new TextElement("Hello PDF", elementPlacement.topCenter, 50, 100);
 		textElement.color = RgbColor.blue;
+        textElement.xOffset = -50;
+        textElement.yOffset = 100
 		textElement.fontSize = 42;
 		pageInput.elements.push(textElement);
+        
 
 
-		var element = new LineElement(elementPlacement.topCenter);
-		element.color = RgbColor.red;
-		//element.xOffset(305);
-		//element.yOffset(150);
-		element.x2Offset = 900;
-		element.y2Offset = 150;
-		element.lineStyle = LineStyle.solid;
-		element.width = 4;
-		pageInput.elements.push(element);
+		var lineElement = new LineElement(elementPlacement.topCenter,200,200);
+		lineElement.color = RgbColor.red;
+        lineElement.xOffset = 305;
+        lineElement.yOffset = 150
+		lineElement.x2Offset = 900;
+		lineElement.y2Offset = 150;
+		lineElement.lineStyle = LineStyle.solid;
+		lineElement.width = 4;
+		pageInput.elements.push(lineElement);
 
+        var recElement = new RectangleElement(elementPlacement.TopCenter, 100, 500);
+        recElement.xOffset = -250;
+        recElement.yOffset = -10;
+        recElement.corner_radius = 10;
+        recElement.border_width = 5;
+        recElement.corner_radius = 10;
+        recElement.border_style = LineStyle.dots;
+        recElement.border_color = RgbColor.blue;
+        recElement.fill_color = RgbColor.green;
+        pageInput.elements.push(recElement);
 
+        var imgResource = new ImageResource(Constants.BasePath + "templates/dynamicpdfLogo.png");
+        var imageElement = new ImageElement(imgResource);
+        imageElement.elementPlacement = elementPlacement.topLeft;
+        imageElement.xOffset = 835;
+        imageElement.yOffset = 75;
+        pageInput.elements.push(imageElement);
 
+        console.log(pdf.instructions);
 
         var res = await pdf.process();
-
 
         if (res.isSuccessful) {
             var outFile = Constants.OutputPath + "solutions-img-text-rec-example-output.pdf";
