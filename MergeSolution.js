cd0e894a1ba79a5d495c2567ec7be2e87d96740b
James A. Brannan <jamesabrannan@hushmail.com>
2024-07-17 10:01:17 -0400

    Create MergeSolution.js

---
+++ MergeSolution.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import {
+    Pdf,
+    PdfResource,
+    ImageResource,
+    LayoutDataResource,
+    HtmlResource,
+    WordResource
+} from "@dynamicpdf/api"
+
+import {Constants} from './constants.js';
+
+export class MergeSolution {
+
+    static async Run() {
+              
+        var pdf = new Pdf();
+        var pdfInput = pdf.addPdf(new PdfResource(Constants.BasePath + "merge-pdfs-pdf-endpoint/DocumentA.pdf"));
+
+
+        var pdfResource = new PdfResource();
+        pdf.addPdf(new PdfResource(Constants.BasePath + "merge-pdfs-pdf-endpoint/DocumentB.pdf"));
+
+        pdf.addWord(new WordResource(Constants.BasePath + "users-guide/Doc1.docx", "Doc1.docx"));
+
+        var imageInput = pdf.addImage(new ImageResource(Constants.BasePath + "image-conversion/testimage.tif"));
+        imageInput.ScaleX = 0.5;
+        imageInput.ScaleY = 0.5;
+        
+        var layoutData = new LayoutDataResource(Constants.BasePath + "creating-pdf-dlex-layout/creating-pdf-dlex-layout.json");
+        pdf.addDlex("samples/creating-pdf-dlex-layout-endpoint/creating-pdf-dlex-layout.dlex", layoutData);
+    
+        var resourcePath = Constants.BasePath + "/users-guide/products.html";
+        pdf.addHtml(new HtmlResource(resourcePath));
+              
+
+        pdf.apiKey =  Constants.ApiKey;
+        
+        var res = await pdf.process();
+        
+        if (res.isSuccessful) {
+            var outFile = Constants.OutputPath + "merge-pdfs-solution-nodejs-output.pdf";
+            var outStream = fs.createWriteStream(outFile);
+            outStream.write(res.content);
+            outStream.close();
+        } else {
+            console.log(res.errorJson);
+        }
+        
+    }
+}
+await MergeSolution.Run();
